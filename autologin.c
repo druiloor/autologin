17a0f51adddf7f9b54138f7de90cde7350fab465
Menno Duursma <druiloor@zonnet.nl>
2020-02-03 02:46:47 +0100

    the flying theepot and stuff

--- autologin.c
+++ autologin.c
@@ -3,19 +3,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <sys/prctl.h>
 
 int main(void)
 {
+        //const char arg2[] = "bob";
+        //const char arg2[] = "connie";
+        //const char arg2[] = "operator";
+        const char arg2[] = "user";
 
 	const char file[] = "/bin/login";
 	const char arg0[] = "login";
 	const char arg1[] = "-f";
-
-	//const char arg2[] = "operator";
-	//const char arg2[] = "bob";
-	//const char arg2[] = "connie";
-	const char arg2[] = "user";
 
 	if (!execlp(file, arg0, arg1, arg2, (char *) NULL)) {
 		fprintf(stderr, "Error: %s - %m\n",
